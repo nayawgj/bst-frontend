fda3b673dfab1a98b279fd5023b1c72c6727fccc
jiyeon0101 <jyk5350@gmail.com>
2023-10-12 00:30:00 +0900

    create book report

--- lib/sidebar/bookmark_list.dart
+++ lib/sidebar/bookmark_list.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class BookMarks extends StatefulWidget {
   const BookMarks({super.key});
 
@@ -44,35 +43,33 @@ class _BookMarksState extends State<BookMarks> {
             ],
           ),
         ),
-        body: Container(
-          child: ListView(
-            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
-            children: [
-              const SizedBox(
-                height: 20,
-              ),
-              Container(
-                height: 90,
-                width: 100,
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  border: Border.all(
-                    width: 1,
-                    color: const Color.fromRGBO(32, 96, 79, 1),
-                  ),
-                  borderRadius: BorderRadius.circular(10),
+        body: ListView(
+          padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+          children: [
+            const SizedBox(
+              height: 20,
+            ),
+            Container(
+              height: 90,
+              width: 100,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                border: Border.all(
+                  width: 1,
+                  color: const Color.fromRGBO(32, 96, 79, 1),
                 ),
-                child: const Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text('책 제목'),
-                    Text('독후감 제목'),
-                  ],
-                ),
+                borderRadius: BorderRadius.circular(10),
               ),
-            ],
-          ),
+              child: const Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('책 제목'),
+                  Text('독후감 제목'),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
